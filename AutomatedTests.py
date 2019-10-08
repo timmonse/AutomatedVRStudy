2fe1e15231c6680aec07008d8a49e7c0e29ae418
Evan Timmons <evangeorgetimmons@gmail.com>
2019-10-08 13:47:10 -0500

    New Gui Code

--- AutomatedTests.py
+++ AutomatedTests.py
@@ -1,18 +1,24 @@
 from subprocess import call
 import random
 import psutil  
+import PySimpleGUI as sg
 
-#Replace the paths with the proper paths
+#The names for the three scene exe files
+#FIX ME Replace the names with the names of the exe files
 read_distance_scene = "VR Read Distance Study.exe"
 foveated_scene = "test2Ex.exe"
 generator_scene = "test3Ex.exe"
 
+#The folder where the exe files are located
+#FIX ME Replace the paths with the proper paths
 folder_path = "C:\\Users\\timmonse\\Desktop\\ReadDistanceStudy\\"
 
+#The combined exe name and folder creating a full path
 read_distance_path = folder_path + read_distance_scene
 foveated_path = folder_path + foveated_scene
 generator_path = folder_path + generator_scene
 
+#List of all three scene paths
 scene_list = []
 scene_list.append(read_distance_scene)
 scene_list.append(foveated_scene)
