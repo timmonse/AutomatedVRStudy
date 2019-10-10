d05b6064719a389637f4fa7c56c72135151608d1
Evan Timmons <evangeorgetimmons@gmail.com>
2019-10-10 13:13:43 -0500

    Added in gui with code
    Not fully functioning yet

--- AutomatedGUI.py
+++ AutomatedGUI.py
@@ -39,23 +39,23 @@ layout = [
 	#Change the default paths
     
 	#Select the read distance study location
-	[sg.Text('File Selection', size=(30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],   
+	[sg.Text('File Selection', size=(20, 1), justification='center', font=("Helvetica", 15), relief=sg.RELIEF_RIDGE)],   
 	
 	[sg.Text('Read Distance Study', size=(35, 1))],      
-	[sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),      
+	[sg.Text('Your File', size=(15, 1), auto_size_text=False, justification='right'),      
 	 sg.InputText("C:\\Users\\timmonse\\Desktop\\ReadDistanceStudy\\Read Distance Study.exe"), sg.FileBrowse()],
 	 
 	#FIX ME ADD NAME OF FOLDER and change the paths
 	[sg.Text('Supriya Study 1', size=(35, 1))],      
-	[sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),      
+	[sg.Text('Your File', size=(15, 1), auto_size_text=False, justification='right'),      
 	 sg.InputText("C:\\Users\\timmonse\\Documents\\Code\\Python\\GitInsidePy\\AutomatedVRStudy\\testEx.exe"), sg.FileBrowse()],
 	 
 	[sg.Text('Supriya Study 2', size=(35, 1))],      
-	[sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),      
+	[sg.Text('Your File', size=(15, 1), auto_size_text=False, justification='right'),      
 	 sg.InputText("C:\\Users\\timmonse\\Documents\\Code\\Python\\GitInsidePy\\AutomatedVRStudy\\test2Ex.exe"), sg.FileBrowse()],
 	 
 	[sg.Text('Supriya Study 3', size=(35, 1))],      
-	[sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),      
+	[sg.Text('Your File', size=(15, 1), auto_size_text=False, justification='right'),      
 	 sg.InputText("C:\\Users\\timmonse\\Documents\\Code\\Python\\GitInsidePy\\AutomatedVRStudy\\test3Ex.exe"), sg.FileBrowse()],
 	 
 	[sg.Submit(tooltip='Click to submit this window'), sg.Cancel()]    
@@ -64,9 +64,43 @@ layout = [
 
 window = sg.Window('VR User Study', layout, default_element_size=(40, 1), grab_anywhere=False)      
 
-event, values = window.Read()      
+event, values = window.Read() 
+
+#Check if want submission
+if event == 'Submit':
+	userID = values[1]
+	read_distance_path = values[2]
+	scene_1_path = values[3]
+	scene_2_path = values[4]
+	scene_3_path = values[5]
+
+	
+window.Close();     
 
 sg.Popup('Title',      
 		 'The results of the window.',      
 		 'The button clicked was "{}"'.format(event),      
-		 'The values are', values)     
+		 'The values are', values)   		 
+
+
+
+
+# !!!!!     END OF GUI     !!!!! !!!!!     END OF GUI     !!!!! !!!!!     END OF GUI     !!!!! !!!!!     END OF GUI     !!!!!
+
+# List of all three scene paths
+# scene_list = []
+# scene_list.append(read_distance_scene)
+# scene_list.append(foveated_scene)
+# scene_list.append(generator_scene)
+
+# #Randomly choose the order to open the scenes and then open them
+# for i in range(3):
+    # rand_limit = 2 - i
+    # rand_scene_pos = random.randint(0,rand_limit)
+
+	# #Open the scene
+    # call([folder_path + scene_list[rand_scene_pos]])
+    # while(scene_list[rand_scene_pos] in (p.name() for p in psutil.process_iter())):
+        # #stay in this loop until the exe has been closed
+        # continue
+    # del scene_list[rand_scene_pos]
