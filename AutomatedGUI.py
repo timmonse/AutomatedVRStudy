125a90a0cb6c4db56f84116739e03c476696f562
Evan Timmons <evangeorgetimmons@gmail.com>
2019-10-10 13:50:19 -0500

    Small updates
    next step is to integrate the exe code with the GUI

--- AutomatedGUI.py
+++ AutomatedGUI.py
@@ -1,4 +1,12 @@
+# File:          AutomatedGUI.py
+# Author:        Evan Timmons
+# Date:          10/10/2019
+# ISU Email:     timmonse@iastate.edu
+# Description:   This program automates the study process for the 2019 ARLAB user study
+
+
 import PySimpleGUI as sg      
+import csv
 
 sg.ChangeLookAndFeel('SystemDefault')      
 
@@ -16,30 +24,19 @@ column1 = [[sg.Text('Column 1', background_color='#F7F3EC', justification='cente
 layout = [      
 	[sg.Menu(menu_def, tearoff=True)],      
 	[sg.Text('VR User Study', size=(30, 1), justification='center', font=("Helvetica", 25), relief=sg.RELIEF_RIDGE)],    
+	[sg.Text('Study Administrator')],      
+	[sg.InputText('Example: Evan Timmons')],
 	[sg.Text('UserID')],      
 	[sg.InputText('Example: U0001')],
-	
-	#[sg.Frame(layout=[      
-	#[sg.Checkbox('Checkbox', size=(10,1)),  sg.Checkbox('My second checkbox!', default=True)],      
-	#[sg.Radio('My first Radio!     ', "RADIO1", default=True, size=(10,1)), sg.Radio('My second Radio!', "RADIO1")]], title='Options',title_color='red', relief=sg.RELIEF_SUNKEN, tooltip='Use these to set flags')],      
-	# [sg.Multiline(default_text='This is the default Text should you decide not to type anything', size=(35, 3)),      
-	 # sg.Multiline(default_text='A second multi-line', size=(35, 3))],      
-	# [sg.InputCombo(('Combobox 1', 'Combobox 2'), size=(20, 1)),      
-	 # sg.Slider(range=(1, 100), orientation='h', size=(34, 20), default_value=85)],      
-	# [sg.InputOptionMenu(('Menu Option 1', 'Menu Option 2', 'Menu Option 3'))],      
-	# [sg.Listbox(values=('Listbox 1', 'Listbox 2', 'Listbox 3'), size=(30, 3)),      
-	 # sg.Frame('Labelled Group',[[      
-	 # sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=25),      
-	 # sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=75),      
-	 # sg.Slider(range=(1, 100), orientation='v', size=(5, 20), default_value=10),      
-	 # sg.Column(column1, background_color='#F7F3EC')]])],   
-	 
+
 	[sg.Text('_'  * 80)], 
 	
 	#Change the default paths
     
 	#Select the read distance study location
 	[sg.Text('File Selection', size=(20, 1), justification='center', font=("Helvetica", 15), relief=sg.RELIEF_RIDGE)],   
+	
+	
 	
 	[sg.Text('Read Distance Study', size=(35, 1))],      
 	[sg.Text('Your File', size=(15, 1), auto_size_text=False, justification='right'),      
@@ -58,6 +55,15 @@ layout = [
 	[sg.Text('Your File', size=(15, 1), auto_size_text=False, justification='right'),      
 	 sg.InputText("C:\\Users\\timmonse\\Documents\\Code\\Python\\GitInsidePy\\AutomatedVRStudy\\test3Ex.exe"), sg.FileBrowse()],
 	 
+	 [sg.Text('_'  * 80)], 
+	 
+    [sg.Text('Output Folder', size=(35, 1))],      
+    [sg.Text('Your Folder', size=(15, 1), auto_size_text=False, justification='right'),      
+	 sg.InputText("C:\\Users\\timmonse\\Desktop\\testOutput\\"), sg.FolderBrowse()],
+	 
+	
+	[sg.Text('_'  * 80)], 
+	 
 	[sg.Submit(tooltip='Click to submit this window'), sg.Cancel()]    
 ]      
 
@@ -68,21 +74,36 @@ event, values = window.Read()
 
 #Check if want submission
 if event == 'Submit':
-	userID = values[1]
-	read_distance_path = values[2]
-	scene_1_path = values[3]
-	scene_2_path = values[4]
-	scene_3_path = values[5]
+	admin_name = values[1]
+	user_ID = values[2]
+	read_distance_path = values[3]
+	scene_1_path = values[4]
+	scene_2_path = values[5]
+	scene_3_path = values[6]
+	output_path = values[7]
+	
+	#Test variables DELETE ME
+	# print(admin_name)
+	# print(user_ID)
+	# print(read_distance_path)
+	# print(scene_1_path)
+	# print(scene_2_path)
+	# print(scene_3_path)
+	# print(output_path)
+	
+	
+
+
 
 	
-window.Close();     
+  
 
-sg.Popup('Title',      
-		 'The results of the window.',      
-		 'The button clicked was "{}"'.format(event),      
-		 'The values are', values)   		 
+# sg.Popup('Title',      
+		 # 'The results of the window.',      
+		 # 'The button clicked was "{}"'.format(event),      
+		 # 'The values are', values)   		 
 
-
+#window.Close()   
 
 
 # !!!!!     END OF GUI     !!!!! !!!!!     END OF GUI     !!!!! !!!!!     END OF GUI     !!!!! !!!!!     END OF GUI     !!!!!
@@ -104,3 +125,15 @@ sg.Popup('Title',
         # #stay in this loop until the exe has been closed
         # continue
     # del scene_list[rand_scene_pos]
+	
+with open(output_path + user_ID + "_main" + ".csv", mode='w', newline='') as output_file:
+    output_writer = csv.writer(output_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+    output_writer.writerow(['Study Admin', 'Participant UserID', 'Time 1', 'Time 2', 'Time 3', 'Time 4',])
+    output_writer.writerow([admin_name, user_ID, 'FIXME', 'FIXME', 'FIXME', 'FIXME'])
+	
+	
+	
+	
+	
+	
